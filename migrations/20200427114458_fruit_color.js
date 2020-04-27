81b3719bc935ca363ac45e24f111a7d3bd232e9b
Amber-Pittman <amber@amberpittman.com>
2020-04-27 11:55:30 -0400

    Able to run 2 migrations in 1 batch

--- migrations/20200427114458_fruit_color.js
+++ migrations/20200427114458_fruit_color.js
@@ -1,12 +1,12 @@
 
 exports.up = async function(knex) {
-  await knex.schema.alterTable("fruit", (table) => {
+  await knex.schema.alterTable("fruits", (table) => {
       table.text("color")
   })
 };
 
 exports.down = async function(knex) {
-    await knex.schema.alterTable("fruit", (table) => {
+    await knex.schema.alterTable("fruits", (table) => {
         table.dropColumn("color")
     })
 };
