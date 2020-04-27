d9007f3644436a6a83b88ec3ab221082072ace79
Amber-Pittman <amber@amberpittman.com>
2020-04-27 10:53:54 -0400

    Created up function for fruits table

--- migrations/20200427104908_fruits.js
+++ migrations/20200427104908_fruits.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("fruits", () => {
+      table.increments("id")
+      table.text("name").notNull().unique()
+      table.float("avgWeightOz").notNull()
+      table.boolean("delicious").notNull().default(true)
+  })
 };
 
-exports.down = function(knex) {
+exports.down = async function(knex) {
   
 };
