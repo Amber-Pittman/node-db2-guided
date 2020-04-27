d61256a6247d36276a654164d1f8209ded803658
Amber-Pittman <amber@amberpittman.com>
2020-04-27 11:49:07 -0400

    Corrected syntax error

--- migrations/20200427114458_fruit_color.js
+++ migrations/20200427114458_fruit_color.js
@@ -1,11 +1,11 @@
 
-exports.up = await function(knex) {
+exports.up = async function(knex) {
   await knex.schema.alterTable("fruit", (table) => {
       table.text("color")
   })
 };
 
-exports.down = await function(knex) {
+exports.down = async function(knex) {
     await knex.schema.alterTable("fruit", (table) => {
         table.dropColumn("color")
     })
